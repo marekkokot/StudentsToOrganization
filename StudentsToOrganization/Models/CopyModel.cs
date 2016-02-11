b1733a67c868a67ce2318d2057e84cda0829a8b9
marekkokot <marek.koki@gmail.com>
2016-02-11 17:22:47 +0100

    missed file

---
+++ StudentsToOrganization/Models/CopyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsToOrganization.Models
+{
+    public class CopyModel
+    {
+        public int Group { get; set; }
+        public int Section { get; set; }
+        public string SrcRepo { get; set; }
+        public string SrcDir { get; set; }
+        public string DestDir { get; set; }
+    }
+}
